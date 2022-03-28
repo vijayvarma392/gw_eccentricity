50197a8ec9d2c6b226b409f9b69b3e56822e02a0
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-03-28 23:34:01 +0530

    complete measure ecc

--- src/eccDefinition.py
+++ src/eccDefinition.py
@@ -6,6 +6,7 @@ Md Arif Shaikh, Mar 28, 2022
 """
 
 import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 
 class eccDefinition:
@@ -16,7 +17,7 @@ class eccDefinition:
 
         parameters:
         ----------
-        datadict: Dictionary conntaining time, modes, etc
+        dataDict: Dictionary conntaining time, modes, etc
         """
         self.dataDict = dataDict
         self.time = self.dataDict["t"]
@@ -45,9 +46,27 @@ class eccDefinition:
 
     def peaks_interp(self, order=10, **kwargs):
         """Interpolator through peaks."""
+        peak_idx = self.find_peaks(order)
+        if len(peak_idx) >= 2:
+            return InterpolatedUnivariateSpline(self.time[peak_idx],
+                                                self.omega22[peak_idx],
+                                                **kwargs)
+        else:
+            print("...Number of peaks is less than 2. Not able"
+                  " to create an interpolator.")
+            return None
 
     def troughs_interp(self, order=10, **kwargs):
         """Interpolator through troughs."""
+        trough_idx = self.find_troughs(order)
+        if len(trough_idx) >= 2:
+            return InterpolatedUnivariateSpline(self.time[trough_idx],
+                                                self.omega22[trough_idx],
+                                                **kwargs)
+        else:
+            print("...Number of troughs is less than 2. Not able"
+                  " to create an interpolator.")
+            return None
 
     def measure_ecc(self, t_ref, order=10, **kwargs):
         """Measure eccentricity and meann anomaly at reference time.
@@ -57,4 +76,36 @@ class eccDefinition:
         t_ref: reference time to measure eccentricity and mean anomaly.
         order: width of peaks/troughs
         kwargs: any extra kwargs to the peak/trough findining functions.
+
+        returns:
+        --------
+        ecc_ref: measured eccentricity at t_ref
+        mean_ano_ref: measured mean anomaly at t_ref
         """
+        default_kwargs = {"w": None,
+                          "bbox": [None, None],
+                          "k": 3,
+                          "ext": 0,
+                          "check_finite": False}
+        for kw in default_kwargs.keys():
+            if kw in kwargs:
+                default_kwargs[kw] = kwargs[kw]
+
+        peaks_interpolator = self.peaks_interp(order, **default_kwargs)
+        troughs_interpolator = self.troughs_interp(order, **default_kwargs)
+
+        if peaks_interpolator is None or troughs_interpolator is None:
+            print("...Sufficiemt number of peaks/troughs are not found."
+                  " Can not creator interpolator. Most probably the "
+                  "excentricity is too small. Returning eccentricity to be"
+                  " zero")
+            ecc_ref = 0
+        else:
+            eccVals = ((np.sqrt(np.abs(peaks_interpolator(self.time)))
+                        - np.sqrt(np.abs(troughs_interpolator(self.time))))
+                       / (np.sqrt(np.abs(peaks_interpolator(self.time)))
+                          + np.sqrt(np.abs(troughs_interpolator(self.times)))))
+            ecc_interpolator = InterpolatedUnivariateSpline(self.time, eccVals)
+            ecc_ref = ecc_interpolator(t_ref)
+
+        return ecc_ref
