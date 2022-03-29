dbb61d13211376c77c91054dd927ff093122922c
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-03-29 18:02:18 +0530

    generate waveform modes

--- src/eccDefinition.py
+++ src/eccDefinition.py
@@ -7,6 +7,8 @@ Md Arif Shaikh, Mar 29, 2022
 
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline
+import lal
+import lalsimulation as lalsim
 
 
 class eccDefinition:
@@ -72,7 +74,7 @@ class eccDefinition:
             return None
 
     def measure_ecc(self, t_ref, order=10, **kwargs):
-        """Measure eccentricity and meann anomaly at reference time.
+        """Measure eccentricity and mean anomaly at reference time.
 
         parameters:
         ----------
@@ -165,3 +167,90 @@ def get_peak_via_quadratic_fit(t, func):
     coefs = np.array([yVec.dot(v1) for v1 in invMat])
     return t[index] - coefs[1]/(2.*coefs[2]), (coefs[0]
                                                - coefs[1]**2./4/coefs[2])
+
+
+#  copied from https://github.com/sxs-collaboration/surrogate_modeling/blob/master/LALModels/LALModels.py#L111
+def generate_waveform_modes(approximant, q, chi1, chi2, deltaTOverM, Momega0,
+                            longAscNodes=0, eccentricity=0, meanPerAno=0,
+                            alignedSpin=True, lambda1=None, lambda2=None):
+    """Generate waveform modes for a given approximant using LALSuite.
+
+    Returns dimless time and dictionary dimless complex strain modes.
+    parameters:
+    ----------
+    approximant     # str, name of approximant
+    q               # float, mass ratio q>=1
+    chi1            # array/list of len=3, dimensionless spin vector of larger BH
+    chi2            # array/list of len=3, dimensionless spin vector of smaller BH
+    deltaTOverM     # float, dimensionless time step size
+    Momega0          # float, dimensionless starting orbital frequency for waveform (rad/s)
+    longAscNodes    # float, Longiture of Ascending nodes
+    eccentricity    # float, Eccentricity
+    meanPerAno      # float, Mean anomaly of periastron
+    alignedSpin     # assume aligned spin approximant
+    lambda1         # tidal parameter for larger BH
+    lambda2         # tidal parameter for smaller BH
+
+    return:
+    ------
+    t               # array, dimensionless time
+    h               # complex array, dimensionless complex strain h_{+} -i*h_{x}
+    """
+    chi1 = np.array(chi1)
+    chi2 = np.array(chi2)
+
+    if alignedSpin:
+        if np.sum(np.sqrt(chi1[:2]**2)) > 1e-5 or np.sum(np.sqrt(chi2[:2]**2)) > 1e-5:
+            raise Exception("Got precessing spins for "
+                            "aligned spin approximant.")
+        if np.sum(np.sqrt(chi1[:2]**2)) != 0:
+            chi1[:2] = 0
+        if np.sum(np.sqrt(chi2[:2]**2)) != 0:
+            chi2[:2] = 0
+
+    # sanity checks
+    if np.sqrt(np.sum(chi1**2)) > 1:
+        raise Exception('chi1 out of range.')
+    if np.sqrt(np.sum(chi2**2)) > 1:
+        raise Exception('chi2 out of range.')
+    if len(chi1) != 3:
+        raise Exception('chi1 must have size 3.')
+    if len(chi2) != 3:
+        raise Exception('chi2 must have size 3.')
+
+    # use M=10 and distance=1 Mpc, but will scale these out before outputting h
+    M = 10      # dimless mass
+    distance = 1.0e6 * lal.PC_SI
+
+    approxTag = lalsim.GetApproximantFromString(approximant)
+    MT = M*lal.MTSUN_SI
+    f_low = Momega0/np.pi/MT
+    f_ref = f_low
+
+    # component masses of the binary
+    m1_kg = M * lal.MSUN_SI * q / (1. + q)
+    m2_kg = M * lal.MSUN_SI / (1. + q)
+
+    # tidal parameters if given
+    if lambda1 is not None or lambda2 is not None:
+        dictParams = lal.CreateDict()
+        lalsim.SimInspiralWaveformParamsInsertTidalLambda1(dictParams, lambda1)
+        lalsim.SimInspiralWaveformParamsInsertTidalLambda2(dictParams, lambda2)
+    else:
+        dictParams = None
+
+    lmax = 5    # This in unused
+    hmodes = lalsim.SimInspiralChooseTDModes(deltaTOverM*MT, m1_kg, m2_kg,
+                                             chi1[0], chi1[1], chi1[2],
+                                             chi2[0], chi2[1], chi2[2],
+                                             f_low, f_ref, distance,
+                                             dictParams, lmax, approxTag)
+
+    t = deltaTOverM * np.arange(len(hmodes.mode.data.data))  # dimensionless time
+    mode_dict = {}
+    while hmodes is not None:
+        mode_dict[f"({hmodes.l}, {hmodes.m})"] \
+            = hmodes.mode.data.data * distance/MT/lal.C_SI
+        hmodes = hmodes.next
+
+    return t, mode_dict
