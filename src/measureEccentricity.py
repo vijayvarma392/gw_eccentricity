65df61ad192ec99c37bb8464acb7cfc1e9e3ecfa
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-03-30 21:39:59 +0530

    documentation

--- src/measureEccentricity.py
+++ src/measureEccentricity.py
@@ -6,14 +6,21 @@ from eccDefinitionUsingFrequencyFits import measureEccentricityUsingFrequencyFit
 from eccDefinitionUsingResidualAmplitude import measureEccentricityUsingResidualAmplitude
 
 
-def measure_eccentricity(method, dataDict, t_ref, height=None, threshold=None,
-                         distance=None, prominence=None, width=10, wlen=None,
-                         rel_height=0.5, plateau_size=None, **kwargs):
+def measure_eccentricity(t_ref, dataDict, method="Amplitude", height=None,
+                         threshold=None, distance=None, prominence=None,
+                         width=10, wlen=None, rel_height=0.5,
+                         plateau_size=None, **kwargs):
     """Measure eccentricity and mean anomaly at reference time.
 
     parameters:
     ----------
     t_ref: reference time to measure eccentricity and mean anomaly.
+    dataDict: dictionary containing waveform modes dict, time etc
+    should follow the format {"t": time, "hlm": modeDict, ..}
+    and modeDict = {(l, m): hlm_mode_data}
+    for ResidualAmplitude method, provide "t0" and "hlm0" as well
+    in the dataDict.
+
     see scipy.signal.find_peaks for rest or the arguments.
     kwargs: to be passed to the InterpolatedUnivariateSpline
 
