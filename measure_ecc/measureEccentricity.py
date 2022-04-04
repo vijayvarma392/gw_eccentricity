f57e93e167c953fd665fb96223954762b3ea5ffc
Vijay Varma <vijay.varma392@gmail.com>
2022-04-04 11:26:59 +0200

    Add version details to measureEccentricity

--- measure_ecc/measureEccentricity.py
+++ measure_ecc/measureEccentricity.py
@@ -1,4 +1,32 @@
-"""Simple script to use different methods and measure eccentricity."""
+"""measureEccentricity
+========
+
+Measure eccentricity and mean anomaly from gravitational waves.
+"""
+__copyright__ = "Copyright (C) 2021 Md Arif Shaikh, Vijay Varma"
+__email__ = "arif.shaikh@icts.res.in, vijay.varma392@gmail.com"
+__status__ = "testing"
+__author__ = "Md Arif Shaikh, Vijay Varma"
+__version__ = "0.1"
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 
 from eccDefinitionUsingAmplitude import eccDefinitionUsingAmplitude
 from eccDefinitionUsingFrequency import eccDefinitionUsingFrequency
