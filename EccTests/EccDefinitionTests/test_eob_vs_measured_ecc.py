11b0733d1a29edfc3722457fc64d618a80fc1733
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-04-19 15:53:09 +0530

    improve documentation, var names #15

--- EccTests/EccDefinitionTests/test_eob_vs_measured_ecc.py
+++ EccTests/EccDefinitionTests/test_eob_vs_measured_ecc.py
@@ -3,12 +3,13 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import argparse
+from argparse import RawTextHelpFormatter
 import warnings
 sys.path.append("../../")
 from measureEccentricity import measure_eccentricity, get_available_methods
 from measureEccentricity.load_data import load_waveform
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
 parser.add_argument(
     "--data_dir",
     type=str,
@@ -19,24 +20,24 @@ parser.add_argument(
     type=str,
     nargs="+",
     default="all",
-    help=("EccDefinition method to test."
-          " Could be one or more of the methods in"
-          " measureEccentricity.get_available_methods."
-          " Default is all."
-          " usage: --method Amplitude Frequency"))
+    help=("EccDefinition method to test.\n"
+          "Could be one or more of the methods in"
+          " measureEccentricity.get_available_methods.\n"
+          "Default is 'all'.\n"
+          "usage: --method 'Amplitude' 'Frequency'"))
 parser.add_argument(
     "--set",
     type=str,
     default="all",
     nargs="+",
-    help=("Run test on the set of parameters. "
-          "Possible choices are one or more of 1, 2, 3, 4 or all."
-          " 1: q=1, chi1z=chi2z=0."
-          " 2: q=2, chi1z=chi2z=0.5"
-          " 3: q=4, chi1z=chi2z=-0.6"
-          " 4: q=6, chi1z=0.4, chi2z=-0.4."
-          " Default is all."
-          " usage: --set 1 2"))
+    help=("Run test on the set of parameters.\n"
+          "Possible choices are one or more of 1, 2, 3, 4 or all.\n"
+          "1: q=1, chi1z=chi2z=0.\n"
+          "2: q=2, chi1z=chi2z=0.5\n"
+          "3: q=4, chi1z=chi2z=-0.6\n"
+          "4: q=6, chi1z=0.4, chi2z=-0.4.\n"
+          "Default is 'all'.\n"
+          "usage: --set 1 2"))
 parser.add_argument(
     "--fig_dir",
     type=str,
@@ -52,13 +53,11 @@ param_sets = {"1": [1, 0, 0],
               "4": [6, 0.4, -0.4]}
 data_dir = args.data_dir + "Non-Precessing/EOB/"
 
-markers = ["o", "v", "^", "<", ">", "d", "+", "x"]
 
-
-def plot_for(method, param_set, marker, ax):
-    MeasuredEccs = []
-    WaveformEccs = []
-    q, chi1z, chi2z = param_sets[param_set]
+def plot_waveform_ecc_vs_model_ecc(method, set_key, ax):
+    waveform_eccs = []
+    model_eccs = []
+    q, chi1z, chi2z = param_sets[set_key]
     for ecc in tqdm(EOBeccs):
         fileName = (f"{data_dir}/EccTest_q{q:.2f}_chi1z{chi1z:.2f}_"
                     f"chi2z{chi2z:.2f}_EOBecc{ecc:.7f}.h5")
@@ -76,36 +75,36 @@ def plot_for(method, param_set, marker, ax):
             tref_out, measured_ecc, mean_ano = measure_eccentricity(tref_in,
                                                                     dataDict,
                                                                     method)
-            MeasuredEccs.append(measured_ecc[0])
-            WaveformEccs.append(ecc)
+            waveform_eccs.append(measured_ecc[0])
+            model_eccs.append(ecc)
         except Exception:
             warnings.warn("Exception raised. Probably too small eccentricity"
                           "to detect any extrema.")
 
-    ax.loglog(WaveformEccs, MeasuredEccs, marker=marker, label=f"{method}")
+    ax.loglog(model_eccs, waveform_eccs, marker="o", label=f"{method}")
 
 
-if args.method[0] == "all":
-    methods = get_available_methods()
-    method_str = args.method[0]
+if "all" in args.method:
+    args.method = get_available_methods()
+    method_str = "all"
 else:
-    methods = args.method
     method_str = "_".join(args.method)
 
-if args.set[0] == "all":
-    sets = param_sets
+if "all" in args.set:
+    args.set = param_sets
 else:
     sets = {}
     for key in args.set:
         sets.update({key: param_sets[key]})
+    args.set = sets
 
 fig_dir = args.fig_dir if args.fig_dir else "./"
 
-for param_set in sets:
+for key in args.set:
     fig, ax = plt.subplots()
-    fig_name = f"{fig_dir}/EccTest_set{param_set}_{method_str}.png"
-    for idx, method in enumerate(methods):
-        plot_for(method, param_set, markers[idx], ax)
+    fig_name = f"{fig_dir}/EccTest_set{set}_{method_str}.png"
+    for idx, method in enumerate(args.method):
+        plot_waveform_ecc_vs_model_ecc(method, key, ax)
     ax.legend()
     ax.grid()
     ax.set_xlabel("EOB Eccentricity")
