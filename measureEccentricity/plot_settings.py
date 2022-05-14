f02eaf56b501b0526de63ddc18efe20c887b2db0
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-05-14 16:51:04 +0530

    change colorlist

--- measureEccentricity/plot_settings.py
+++ measureEccentricity/plot_settings.py
@@ -1,14 +1,37 @@
 """Fancy settings for plots."""
 from matplotlib import rc
 from cycler import cycler
-from palettable.wesanderson import GrandBudapest3_6
-from palettable.wesanderson import GrandBudapest4_5
+from palettable.wesanderson import Aquatic1_5
+from palettable.wesanderson import Darjeeling2_5
 from palettable.wesanderson import Darjeeling3_5
+from palettable.wesanderson import FantasticFox2_5
+from palettable.wesanderson import GrandBudapest5_5
+from palettable.wesanderson import GrandBudapest1_4
+from palettable.wesanderson import GrandBudapest4_5
+from palettable.wesanderson import GrandBudapest3_6
+from palettable.wesanderson import Zissou_5
+from palettable.wesanderson import Royal1_4
+colors_aq_15 = Aquatic1_5.mpl_colors
+colors_dj_25 = Darjeeling2_5.mpl_colors
+colors_dj_35 = Darjeeling3_5.mpl_colors
+colors_ff_25 = FantasticFox2_5.mpl_colors
+colors_gb_55 = GrandBudapest5_5.mpl_colors
+colors_gb_14 = GrandBudapest1_4.mpl_colors
+colors_gb_45 = GrandBudapest4_5.mpl_colors
+colors_gb_36 = GrandBudapest3_6.mpl_colors
+colors_zs_5 = Zissou_5.mpl_colors
+colors_ry_14 = Royal1_4.mpl_colors
 
-gb36 = GrandBudapest3_6.mpl_colors
-gb45 = GrandBudapest4_5.mpl_colors
-d35 = Darjeeling3_5.mpl_colors
-colorList = [gb36[1], gb36[2], d35[4], gb36[4], gb36[5], gb45[1]]
+colorsList = [colors_zs_5[2],
+              colors_gb_14[2],
+              colors_gb_45[3],
+              colors_aq_15[1],
+              colors_zs_5[0],
+              colors_gb_55[1],
+              colors_aq_15[0],
+              colors_aq_15[0],
+              colors_dj_25[3],
+              colors_dj_25[1]]
 
 
 def use_fancy_plotsettings():
@@ -16,4 +39,4 @@ def use_fancy_plotsettings():
     # LaTeX
     rc("text", usetex=True)
     # color cycler
-    rc("axes", prop_cycle=cycler(color=colorList))
+    rc("axes", prop_cycle=cycler(color=colorsList))
