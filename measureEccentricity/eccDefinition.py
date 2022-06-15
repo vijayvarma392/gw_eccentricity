74b5c1b6d14ada712dc66005e25cbfe47f54fc73
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-06-15 23:52:14 +0530

    update check for alternating peaks and troughs

--- measureEccentricity/eccDefinition.py
+++ measureEccentricity/eccDefinition.py
@@ -272,7 +272,7 @@ class eccDefinition:
             self.omega22_troughs_interp, self.troughs_location = self.interp_extrema("minima")
 
         # check that peaks and troughs are appearing alternatively
-        self.check_peaks_and_troughs_appear_alternatively()
+        self.check_peaks_and_troughs_appear_alternatingly()
 
         t_peaks = self.t[self.peaks_location]
         t_troughs = self.t[self.troughs_location]
@@ -470,68 +470,56 @@ class eccDefinition:
             if any(self.d2ecc_dt > 0):
                 warnings.warn("Ecc(t) is concave.")
 
-    def check_peaks_and_troughs_appear_alternatively(self):
-        """Check that peaks and troughs appear alternatively."""
-        # if peaks and troughs appear alternatively, then the number
+    def check_peaks_and_troughs_appear_alternatingly(self):
+        """Check that peaks and troughs appear alternatingly."""
+        # if peaks and troughs appear alternatingly, then the number
         # of peaks and troughs should differ by one.
         if abs(len(self.peaks_location) - len(self.troughs_location)) >= 2:
             warnings.warn(
                 "Number of peaks and number of troughs differ by "
                 f"{abs(len(self.peaks_location) - len(self.troughs_location))}"
                 ". This implies that peaks and troughs are not appearing"
-                " alternatively.")
-        if len(self.peaks_location) == len(self.troughs_location):
-            # First we get an array of difference in time of consecutive
-            # peak and trough
-            t_diff = (self.t[self.peaks_location]
-                      - self.t[self.troughs_location])
-            # if the peaks and troughs does not appear alternatively,
-            # then there would be change of sign in t_diff
-            if any((t_diff[:-1] * t_diff[1:]) < 0):
+                " alternatingly.")
+        else:
+            # If the number of peaks and troughs differ by zero or one then we
+            # do the following:
+            if len(self.peaks_location) == len(self.troughs_location):
+                # Check the time of the first peak and the first trough
+                # whichever comes first is assigned as arr1 and the other one
+                # as arr2
+                if self.t[self.peaks_location][0] < self.t[self.troughs_location][0]:
+                    arr1 = self.peaks_location
+                    arr2 = self.troughs_location
+                else:
+                    arr2 = self.peaks_location
+                    arr1 = self.troughs_location
+            else:
+                # Check the number of peaks and troughs
+                # whichever is larger is assigned as arr1 and the other one as
+                # arr2
+                if len(self.peaks_location) > len(self.troughs_location):
+                    arr1 = self.peaks_location
+                    arr2 = self.troughs_location
+                else:
+                    arr2 = self.peaks_location
+                    arr1 = self.troughs_location
+            # create a new array which takes elements from arr1 and arr2
+            # alternatingly
+            arr = np.zeros(arr1.shape[0] + arr2.shape[0], dtype=arr1.dtype)
+            # assign every other element to values from arr1 starting from
+            # index = 0
+            arr[::2] = arr1
+            # assign every other element to values from arr2 starting from
+            # index = 1
+            arr[1::2] = arr2
+            # get the time difference between consecutive locations in arr
+            t_diff = np.diff(self.t[arr])
+            # If peaks and troughs appear alternatingly then all the time
+            # differences in t_diff should be positive
+            if any(t_diff < 0):
                 warnings.warn(
                     "There is at least one instance where "
-                    "peaks and troughs do not appear alternatively.")
-
-        # Only remaining option is where the number of peaks and troughs
-        # differ by one
-        if len(self.peaks_location) == (len(self.troughs_location) + 1):
-            # Each trough must be surrounded by two peaks.
-            # To check that we take a trough location and
-            # get the time differnce from the previous peak to
-            # this trough and the time difference between this trough
-            # to the next peak.
-            # The sign of time difference should be the same.
-            # We do it in one go in the following way
-            t_diff_previous_peak_to_trough = (
-                self.t[self.troughs_location]
-                - self.t[self.peaks_location[:-1]])
-            t_diff_trough_to_next_peak = (
-                self.t[self.peaks_location[1:]]
-                - self.t[self.troughs_location])
-            if any((t_diff_previous_peak_to_trough
-                    * t_diff_trough_to_next_peak) < 0):
-                warnings.warn(
-                    "There is at least one instance where "
-                    "peaks and troughs do not appear alternatively.")
-        if len(self.peaks_location) == (len(self.troughs_location) - 1):
-            # Each peak must be surrounded by two troughs.
-            # To check that we take a peak location and
-            # get the time differnce from the previous trough to
-            # this peak and the time difference between this peak
-            # to the next trough.
-            # The sign of time difference should be the same.
-            # We do it in one go in the following way
-            t_diff_previous_trough_to_peak = (
-                self.t[self.peaks_location]
-                - self.t[self.troughs_location[:-1]])
-            t_diff_peak_to_next_trough = (
-                self.t[self.troughs_location[1:]]
-                - self.t[self.peaks_location])
-            if any((t_diff_previous_trough_to_peak
-                    * t_diff_peak_to_next_trough) < 0):
-                warnings.warn(
-                    "There is at least one instance where "
-                    "peaks and troughs do not appear alternatively.")
+                    "peaks and troughs do not appear alternatingly.")
 
     def compute_res_amp_and_omega22(self):
         """Compute residual amp22 and omega22."""
