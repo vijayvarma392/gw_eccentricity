c38716be56a85453474732f7e9431291ebc1a6e1
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-06-16 23:09:33 +0530

    make changes in __init__

--- gw_eccentricity/__init__.py
+++ gw_eccentricity/__init__.py
@@ -1,10 +1,10 @@
-from . import measureEccentricity
-__author__ = measureEccentricity.__author__
-__email__ = measureEccentricity.__email__
-__copyright__ = measureEccentricity.__copyright__
-__license__ = measureEccentricity.__license__
-__version__ = measureEccentricity.__version__
-__doc__ = measureEccentricity.__doc__
+from . import gw_eccentricity
+__author__ = gw_eccentricity.__author__
+__email__ = gw_eccentricity.__email__
+__copyright__ = gw_eccentricity.__copyright__
+__license__ = gw_eccentricity.__license__
+__version__ = gw_eccentricity.__version__
+__doc__ = gw_eccentricity.__doc__
 
-from .measureEccentricity import measure_eccentricity
-from .measureEccentricity import get_available_methods
+from .gw_eccentricity import measure_eccentricity
+from .gw_eccentricity import get_available_methods
