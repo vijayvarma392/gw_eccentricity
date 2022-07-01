be1f926fde9c9b5e81fa3f41dda30832a7330286
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-07-01 16:41:27 +0530

    move load data outside method loop

--- EccTests/EOBTests/eob_smoothness_test.py
+++ EccTests/EOBTests/eob_smoothness_test.py
@@ -1,3 +1,5 @@
+"""Test smoothness of eccentricity definition methods using EOB waveforms."""
+
 __doc__ = """This test checks whether different eccentricity definitions vary
 smoothly as a function of the internal eccentricity definition used by Toni's
 EOB model. The EOB waveforms that are used for this test are generated from a
@@ -8,16 +10,18 @@ smoothness of the measured eccentricities compared to the eob eccentricities
 used by the waveform model to generate the waveforms in the following two ways:
 
 - For each waveform, we measure the eccentricity at the very first extrema
-(periastron or apastron). That way, the measured eccentricity is also at (nearly)
-Momega0. Finally, we plot the measured eccentricity vs the internal EOB eccentricity.
-You should check visually whether there are glitchy features in these plots.
+(pericenter or apocenter). That way, the measured eccentricity is also at
+(nearly) Momega0. Finally, we plot the measured eccentricity vs the internal
+EOB eccentricity.  You should check visually whether there are glitchy features
+in these plots.
 
 - For each waveform, We plot the measured eccentricity vs tref_out, and color
 the lines by the input EOB eccentricity at Momega0. You should check visually
 whether there are glitchy features in these plots.
 
 Usage:
-python eob_smoothness_test.py -d ecc_waveforms -m 'Amplitude' 'ResidualAmplitude' -p 'all'
+python eob_smoothness_test.py -d ecc_waveforms -m 'Amplitude' \
+'ResidualAmplitude' -p 'all'
 python eob_smoothness_test.py -d ecc_waveforms -m 'all' -p 'all'
 """
 
@@ -88,12 +92,13 @@ parser.add_argument(
     "--slice",
     type=int,
     default=1,
-    help=("Slice the eccentricities to loop over (EOBecccs array containing 150 eccs from 1e-7 to 1.0)"
-          " by taking only every nth ecc value given by slice."
-          " This is useful when we want do not want to plot for all"
-          " the ecc but skip n number of ecc given by slice. NOTE: This is applied only"
-          " for the measured ecc vs time plot to make each line readable. Specially"
-          " for presenting in the paper. For regular test using the default value, i.e.,"
+    help=("Slice the eccentricities to loop over (EOBecccs array containing"
+          " 150 eccs from 1e-7 to 1.0) by taking only every nth ecc value"
+          " given by slice. This is useful when we do not want to plot"
+          " for all the ecc but skip n number of ecc given by slice."
+          " NOTE: This is applied only for the measured ecc vs time plot to"
+          " make each line readable. Specially for presenting in the paper."
+          " For regular test using the default value, i.e.,"
           " no slicing is recommended."))
 parser.add_argument(
     "--paper",
@@ -104,9 +109,11 @@ args = parser.parse_args()
 
 EOBeccs = 10**np.linspace(-7, 0, 150)
 Momega0 = 0.01
-# We generate the waveforms with initial mean anomaly = pi/2 so that over the entire range
-# of initial eccentricities 1e-7 to 1, the length of generated waveforms (inspiral time)
-# does not change too much which is the case if we start with mean anomaly = 0. 
+# We generate the waveforms with initial mean anomaly = pi/2 so that over the
+# entire range of initial eccentricities 1e-7 to 1, the length of generated
+# waveforms (inspiral time) does not change too much which is the case if we
+# start with mean anomaly = 0. This is because at mean_ano = pi/2 the
+# instantaneous frequency is close to the orbit averaged frequency.
 meanAno = np.pi/2
 Momega0_zeroecc = 0.002
 # Format: [q, chi1z, chi2z]
@@ -127,6 +134,7 @@ colors = cmap(np.linspace(0, 1, len(EOBeccs)))
 
 
 def plot_waveform_ecc_vs_model_ecc(methods, key):
+    """Create plots for given methods and parameter set."""
     # Get the output figure name
     # method_str is used in the filename for the output figure
     if "all" in methods:
@@ -136,31 +144,36 @@ def plot_waveform_ecc_vs_model_ecc(methods, key):
         method_str = "_".join(args.method)
 
     if args.example:
-        fig_ecc_at_start_name = (f"{args.fig_dir}/test_eob_vs_measured_ecc_example"
-                                        f".{args.plot_format}")
-        fig_ecc_vs_t_name = (f"{args.fig_dir}/test_measured_ecc_vs_time_example."
-                    f"{args.plot_format}")
+        fig_ecc_at_start_name = (
+            f"{args.fig_dir}/test_eob_vs_measured_ecc_example"
+            f".{args.plot_format}")
+        fig_ecc_vs_t_name = (
+            f"{args.fig_dir}/test_measured_ecc_vs_time_example."
+            f"{args.plot_format}")
     else:
-        fig_ecc_at_start_name = (f"{args.fig_dir}/test_eob_vs_measured_ecc_set{key}_{method_str}"
-                                   f".{args.plot_format}")
-        fig_ecc_vs_t_name = (f"{args.fig_dir}/test_measured_ecc_vs_time_set{key}_{method_str}"
-                                         f".{args.plot_format}")
+        fig_ecc_at_start_name = (
+            f"{args.fig_dir}/test_eob_vs_measured_ecc_set{key}_{method_str}"
+            f".{args.plot_format}")
+        fig_ecc_vs_t_name = (
+            f"{args.fig_dir}/test_measured_ecc_vs_time_set{key}_{method_str}"
+            f".{args.plot_format}")
     # For eob vs measured ecc at the first extrema
     fig_ecc_at_start, ax_ecc_at_start = plt.subplots(
-        figsize=(figWidthsOneColDict[journal], 3))
+        figsize=(figWidthsOneColDict[style], 3))
     # For measured ecc vs time
     fig_ecc_vs_t, ax_ecc_vs_t = plt.subplots(
         nrows=len(methods),
-        figsize=(figWidthsOneColDict[journal], # Figure width 
-                 (2 if args.paper else 3) # Height of each row
-                 * len(methods) # Number of rows
+        figsize=(figWidthsOneColDict[style],  # Figure width
+                 (2 if args.paper else 3)  # Height of each row
+                 * len(methods)  # Number of rows
                  ))
     if len(methods) == 1:
         ax_ecc_vs_t = [ax_ecc_vs_t]
     # Create dictionaries to store different quantities for each methods
     # as we loop over all the eccentricity and methods.
-    model_eccs = {} # To keep track of eob input eccentricity.
-    measured_eccs_at_start = {}  # To keep track of measured ecc at the first extrema.
+    model_eccs = {}  # To keep track of eob input eccentricity.
+    measured_eccs_at_start = {}  # To keep track of measured ecc at the first
+    # extrema.
     tmaxList = {}  # To keep track of minimum time in tref_out across all eccs.
     tminList = {}  # To keep track of maximum time in tref_out across all eccs.
     # Initiate the dictionary with an empty list for each method
@@ -175,19 +188,19 @@ def plot_waveform_ecc_vs_model_ecc(methods, key):
                     f"chi2z{chi2z:.2f}_EOBecc{ecc:.10f}_"
                     f"Momega0{Momega0:.3f}_meanAno{meanAno:.3f}.h5")
         kwargs = {"filepath": fileName}
+        # check if any residual method is in methods. If yes then load
+        # zeroecc waveforms also.
+        if "ResidualAmplitude" in methods or "ResidualFrequency" in methods:
+            fileName_zero_ecc = (
+                f"{data_dir}/EccTest_q{q:.2f}_chi1z"
+                f"{chi1z:.2f}_"
+                f"chi2z{chi2z:.2f}_EOBecc{0:.10f}_"
+                f"Momega0{Momega0_zeroecc:.3f}_meanAno{meanAno:.3f}.h5")
+            kwargs.update({"filepath_zero_ecc": fileName_zero_ecc,
+                           "include_zero_ecc": True})
         dataDict = load_waveform(catalog="EOB", **kwargs)
         tref_in = dataDict["t"]
         for idx, method in enumerate(methods):
-            if "Residual" in method:
-                fileName_zero_ecc = (
-                    f"{data_dir}/EccTest_q{q:.2f}_chi1z"
-                    f"{chi1z:.2f}_"
-                    f"chi2z{chi2z:.2f}_EOBecc{0:.10f}_"
-                    f"Momega0{Momega0_zeroecc:.3f}_meanAno{meanAno:.3f}.h5")
-                kwargs.update({"filepath_zero_ecc": fileName_zero_ecc,
-                               "include_zero_ecc": True})
-                # Reload the datadict to include zeroecc data
-                dataDict = load_waveform(catalog="EOB", **kwargs)
             try:
                 tref_out, measured_ecc, mean_ano = measure_eccentricity(
                     tref_in=tref_in,
@@ -201,20 +214,25 @@ def plot_waveform_ecc_vs_model_ecc(methods, key):
                 measured_eccs_at_start[method] += [measured_ecc[0]]
                 tmaxList[method] += [tref_out[-1]]
                 tminList[method] += [tref_out[0]]
-                # Add measured ecc vs time plot for each method to corresponding axis.
-                # Add only for idx0 that are multiples of args.slice. This is to reduce the
-                # number of lines in the plot to make each line plot visible
+                # Add measured ecc vs time plot for each method to
+                # corresponding axis.  Add only for idx0 that are multiples of
+                # args.slice. This is to reduce the number of lines in the plot
+                # to make each line plot visible. This is true only for this
+                # plot.  For ecc at first extrema vs eob eccs, we plot for all
+                # eccs.
                 if idx0 % args.slice == 0:
-                    ax_ecc_vs_t[idx].plot(tref_out, measured_ecc, c=colors[idx0], label=f"{ecc:.7f}")
+                    ax_ecc_vs_t[idx].plot(tref_out, measured_ecc,
+                                          c=colors[idx0], label=f"{ecc:.7f}")
                 if idx == len(methods) - 1:
                     ax_ecc_vs_t[idx].set_xlabel(r"$t$ [$M$]")
             except Exception:
-                warnings.warn("Exception raised. Probably too small eccentricity "
-                              "to detect any extrema.")
-    # Iterate over methods to plots measured ecc at the first extrema vs eob eccs collected above
-    # looping over all EOB eccs for each methods
+                warnings.warn("Exception raised. Probably too small"
+                              " eccentricity to detect any extrema.")
+    # Iterate over methods to plots measured ecc at the first extrema vs eob
+    # eccs collected above looping over all EOB eccs for each methods
     for idx, method in enumerate(methods):
-        # Plot measured eccs at the first extrema vs eob eccs for different methods
+        # Plot measured eccs at the first extrema vs eob eccs for different
+        # methods
         ax_ecc_at_start.loglog(
             model_eccs[method], measured_eccs_at_start[method], label=method,
             c=colorsDict.get(method, "C0"),
@@ -238,7 +256,8 @@ def plot_waveform_ecc_vs_model_ecc(methods, key):
         ax_ecc_vs_t[idx].set_yticks(10.0**np.arange(-6.0, 1.0, 2.0))
         # Add text indicating the method used
         ax_ecc_vs_t[idx].text(0.95, 0.95, method, ha="right", va="top",
-                transform=ax_ecc_vs_t[idx].transAxes, fontsize=10)
+                              transform=ax_ecc_vs_t[idx].transAxes,
+                              fontsize=10)
         # Add colorbar
         norm = mpl.colors.LogNorm(vmin=EOBeccs.min(), vmax=EOBeccs.max())
         divider = make_axes_locatable(ax_ecc_vs_t[idx])
@@ -251,9 +270,10 @@ def plot_waveform_ecc_vs_model_ecc(methods, key):
         cbar.set_label(r"$e_{\mathrm{EOB}}$ at $\omega_0$",
                        size=10)
         if idx == 0:
-            ax_ecc_vs_t[idx].set_title(rf"$q={q:.1f}, \chi_{{1z}}={chi1z:.1f}, "
-                         rf"\chi_{{2z}}={chi2z:.1f}$",
-                         ha="center", fontsize=10)
+            ax_ecc_vs_t[idx].set_title(
+                rf"$q={q:.1f}, \chi_{{1z}}={chi1z:.1f}, "
+                rf"\chi_{{2z}}={chi2z:.1f}$",
+                ha="center", fontsize=10)
 
     # Customize measured eccs at the first extrema vs eob eccs
     ax_ecc_at_start.set_title(
@@ -283,12 +303,13 @@ def plot_waveform_ecc_vs_model_ecc(methods, key):
         f"{fig_ecc_vs_t_name}",
         bbox_inches="tight")
 
+
 if "all" in args.param_set_key:
     args.param_set_key = list(available_param_sets.keys())
 
 #  Use fancy colors and other settings
-journal = "APS" if args.paper else "Notebook"
-use_fancy_plotsettings(journal=journal)
+style = "APS" if args.paper else "Notebook"
+use_fancy_plotsettings(style=style)
 
 for key in args.param_set_key:
     plot_waveform_ecc_vs_model_ecc(args.method, key)
