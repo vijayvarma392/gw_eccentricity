541a49a93a9a0af0206aa6841a4a84ac11dca689
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-07-29 10:20:49 +0530

    improve documentation

--- gw_eccentricity/truncate_waveform_by_flow.py
+++ gw_eccentricity/truncate_waveform_by_flow.py
@@ -5,25 +5,60 @@ import copy
 
 
 def truncate_waveform_by_flow(dataDict=None,
-                              method="Amplitude",
                               flow=None,
+                              method="Amplitude",
                               spline_kwargs=None,
                               extra_kwargs=None):
     """Truncate waveform by flow.
 
     Eccentric waveforms have a non-monotonic instantaneous frequency.
-    Therefore, to truncate waveform by demanding the frequency of the waveform
-    is greater than a given minimum frequency, say flow, should be done
-    carefully since the instantaneous frequency can be equal to the given flow
-    at multiple points in time.
+    Therefore, truncating waveform by demanding that the frequency of the
+    truncated waveform should be greater than a given minimum frequency, say
+    flow, must be done carefully since the instantaneous frequency can be equal
+    to the given flow at multiple points in time.
 
-    Also by demanding that the truncated waveform has frequencies that are
+    By demanding that the truncated waveform has frequencies that are
     greater than or equal to the flow, we mean that at all times in the
     truncated waveform the frequencies are >= flow. This could be done by using
     the frequency interpolant omega22_a(t) through the apocenters because
     1. It is monotonic function of time.
-    2. If at any time tref, omega22_a(tref) >= 2*pi*flow, then for t >= tref,
+    2. If at a time tlow, omega22_a(tlow) = 2*pi*flow, then for any t > tlow,
        frequencies would be > flow.
+
+    Thus, we find tlow such that omega22_a(tlow) = 2*pi*flow and truncate the
+    waveform by retaing only the part where t >= tlow.
+
+    Paramerers:
+    -----------
+    datadict: dict
+        Dictionary containing waveform data in the following format:
+        dataDict = {"t": t,
+                    "hlm": hlm},
+        where t is the time array and hlm is a dictionary of waveform modes
+        of the following format:
+        hlm = {(l, m): lm_mode}
+    flow: float
+        Lower cutoff frequency to truncate the given waveform modes.
+        The truncated waveform would have frequencies >= flow.
+    method: str
+        Method to find the locations of the apocenters.
+        See gw_eccentricity.get_available_modes for available modes.
+    spline_kwargs: dict
+        Dictionary of arguments to be provided the the
+        scipy.interpolate.InterpolatedUnivariatespline interpolant
+        to create an interpolant of omega22 at the apocenters.
+        Default values are set using eccDefinition.get_default_spline_kwargs.
+    extra_kwargs: dict
+        Dictionary of arguments that might be used for extrema finding routine.
+        Default values are set using eccDefinition.get_default_extra_kwargs.
+
+    Returns:
+    --------
+    truncatedDict: dict
+        Dictionary containing the truncated waveform.
+        Has the same type as dataDict.
+    gwecc_object: obj
+        Object used to truncate the dataDict.
     """
     if dataDict is None:
         raise ValueError("dataDict can not be None.")
@@ -72,6 +107,7 @@ def truncate_waveform_by_flow(dataDict=None,
             = truncatedDict["hlm"][mode][truncatedDict["t"] >= tlow]
     truncatedDict["t"] = truncatedDict["t"][truncatedDict["t"] >= tlow]
 
+    gwecc_object.method = method
     gwecc_object.tlow_for_trucating = tlow
     gwecc_object.truncatedDict = truncatedDict
     gwecc_object.f_low_for_truncating = flow
