2d45d1fc72f94a1aaeac203479ca5426504f226f
md-arif-shaikh <arifshaikh.astro@gmail.com>
2022-11-24 17:09:48 +0530

    separate regression data for each method

--- test/generate_regression_data.py
+++ test/generate_regression_data.py
@@ -1,21 +1,27 @@
-"""Generate data for regression test."""
+"""Generate data for regression test.
+
+Run it from the base directory.
+"""
 import numpy as np
 import json
-import subprocess
 import os
-import sys
-git_home = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'],
-                                   text=True).strip('\n')
-sys.path.append(git_home)
+import argparse
 import gw_eccentricity
 from gw_eccentricity import load_data
 from gw_eccentricity import measure_eccentricity
 
-data_dir = git_home + "/test/regression_data/"
+data_dir = "test/regression_data/"
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--method", "-m",
+    type=str,
+    required=True,
+    help="EccDefinition method to save the regression data for.")
+args = parser.parse_args()
 
 
-def generate_regression_data():
-    """Generate data for regression test using all methods."""
+def generate_regression_data(method):
+    """Generate data for regression test using a method."""
     # Load test waveform
     lal_kwargs = {"approximant": "EccentricTD",
                   "q": 1.0,
@@ -32,46 +38,50 @@ def generate_regression_data():
 
     # List of all available methods
     available_methods = gw_eccentricity.get_available_methods()
+    if method not in available_methods:
+        raise Exception(f"method {method} is not available. Must be one of "
+                        f"{available_methods}")
+
     extra_kwargs = {"debug": False}
+    user_kwargs = extra_kwargs.copy()
     regression_data.update({"extra_kwargs": extra_kwargs})
-    for method in available_methods:
-        # Try evaluating at an array of times
-        gwecc_dict = measure_eccentricity(
-            tref_in=dataDict["t"],
-            method=method,
-            dataDict=dataDict,
-            extra_kwargs=extra_kwargs)
-        tref_out = gwecc_dict["tref_out"]
-        ecc_ref = gwecc_dict["eccentricity"]
-        meanano_ref = gwecc_dict["mean_anomaly"]
-        # For each method we save the measured data 3 reference times
-        n = len(tref_out)
-        dict_tref =  {"time": [tref_out[0], tref_out[n//4], tref_out[n//2]],
-                      "eccentricity": [ecc_ref[0], ecc_ref[n//4], ecc_ref[n//2]],
-                      "mean_anomaly": [meanano_ref[0], meanano_ref[n//4], meanano_ref[n//2]]}
+    # Try evaluating at an array of times
+    gwecc_dict = measure_eccentricity(
+        tref_in=dataDict["t"],
+        method=method,
+        dataDict=dataDict,
+        extra_kwargs=user_kwargs)
+    tref_out = gwecc_dict["tref_out"]
+    ecc_ref = gwecc_dict["eccentricity"]
+    meanano_ref = gwecc_dict["mean_anomaly"]
+    # We save the measured data 3 reference times
+    n = len(tref_out)
+    dict_tref =  {"time": [tref_out[0], tref_out[n//4], tref_out[n//2]],
+                  "eccentricity": [ecc_ref[0], ecc_ref[n//4], ecc_ref[n//2]],
+                  "mean_anomaly": [meanano_ref[0], meanano_ref[n//4], meanano_ref[n//2]]}
         
-        # Try evaluating at an array of frequencies
-        gwecc_dict = measure_eccentricity(
-            fref_in=np.arange(0.025, 0.035, 0.001) / (2 * np.pi),
-            method=method,
-            dataDict=dataDict,
-            extra_kwargs=extra_kwargs)
-        fref_out = gwecc_dict["fref_out"]
-        ecc_ref = gwecc_dict["eccentricity"]
-        meanano_ref = gwecc_dict["mean_anomaly"]
-        n = len(fref_out)
-        dict_fref = {"frequency": [fref_out[0], fref_out[n//4], fref_out[n//2]],
-                     "eccentricity": [ecc_ref[0], ecc_ref[n//4], ecc_ref[n//2]],
-                     "mean_anomaly": [meanano_ref[0], meanano_ref[n//4], meanano_ref[n//2]]}
-        regression_data.update({method: {"tref": dict_tref,
-                                         "fref": dict_fref}})
+    # Try evaluating at an array of frequencies
+    gwecc_dict = measure_eccentricity(
+        fref_in=np.arange(0.025, 0.035, 0.001) / (2 * np.pi),
+        method=method,
+        dataDict=dataDict,
+        extra_kwargs=user_kwargs)
+    fref_out = gwecc_dict["fref_out"]
+    ecc_ref = gwecc_dict["eccentricity"]
+    meanano_ref = gwecc_dict["mean_anomaly"]
+    n = len(fref_out)
+    dict_fref = {"frequency": [fref_out[0], fref_out[n//4], fref_out[n//2]],
+                 "eccentricity": [ecc_ref[0], ecc_ref[n//4], ecc_ref[n//2]],
+                 "mean_anomaly": [meanano_ref[0], meanano_ref[n//4], meanano_ref[n//2]]}
+    regression_data.update({"tref": dict_tref,
+                            "fref": dict_fref})
 
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
     # save to a json file
-    fl = open(f"{data_dir}/regression_data.json", "w")
+    fl = open(f"{data_dir}/{method}_regression_data.json", "w")
     json.dump(regression_data, fl)
     fl.close()
 
 # generate regression data
-generate_regression_data()
+generate_regression_data(args.method)
--- test/test_regression.py
+++ test/test_regression.py
@@ -7,31 +7,31 @@ import gw_eccentricity
 from gw_eccentricity import load_data
 from gw_eccentricity import measure_eccentricity
 
-git_home = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'],
-                                   text=True).strip('\n')
+# locally it passs without problem but on github action, we need to set this tolerance
+atol = 1e-11
 
 def test_regression():
     """Regression test using all methods."""
-    # Load regression data
-    regression_data_file = f"{git_home}/test/regression_data/regression_data.json"
-    if not os.path.exists(regression_data_file):
-        raise Exception(f"Regression data not found at {regression_data_file}\n"
-                        "You may generate it using `generate_regression_data.py`")
-    # Load the regression data
-    fl = open(regression_data_file, "r")
-    regression_data = json.load(fl)
-    fl.close()
-    # waveform kwargs
-    lal_kwargs = regression_data["waveform_kwargs"]
-    # load waveform data
-    dataDict = load_data.load_waveform(**lal_kwargs)
-
     # List of all available methods
     available_methods = gw_eccentricity.get_available_methods()
-    extra_kwargs = regression_data["extra_kwargs"]
     for method in available_methods:
+        # Load regression data
+        regression_data_file = f"test/regression_data/{method}_regression_data.json"
+        if not os.path.exists(regression_data_file):
+            raise Exception(f"Regression data not found at {regression_data_file}\n"
+                            "You may generate it using `generate_regression_data.py`")
+        # Load the regression data
+        fl = open(regression_data_file, "r")
+        regression_data = json.load(fl)
+        fl.close()
+        # waveform kwargs
+        lal_kwargs = regression_data["waveform_kwargs"]
+        # load waveform data
+        dataDict = load_data.load_waveform(**lal_kwargs)
+        extra_kwargs = regression_data["extra_kwargs"]
+
         # Try evaluating at times where regression data are saved
-        regression_data_at_tref = regression_data[method]["tref"]
+        regression_data_at_tref = regression_data["tref"]
         tref_in = regression_data_at_tref["time"]
         gwecc_dict = measure_eccentricity(
             tref_in=tref_in,
@@ -44,17 +44,16 @@ def test_regression():
         # Compare the measured data with the saved data
         np.testing.assert_allclose(
             ecc_ref, regression_data_at_tref["eccentricity"],
+            atol=atol,
             err_msg="measured and saved eccentricity at saved times do not match.")
-        # locally it pass without problem but on github action need to set these tolerance
-        mean_anomaly_atol = 1e-11
         np.testing.assert_allclose(
             meanano_ref, regression_data_at_tref["mean_anomaly"],
-            atol=mean_anomaly_atol,
+            atol=atol,
             err_msg="measured and saved mean anomaly at saved times do not match.")
         
         
         # Try evaluating at frequencies where regression data are saved
-        regression_data_at_fref = regression_data[method]["fref"]
+        regression_data_at_fref = regression_data["fref"]
         fref_in = regression_data_at_fref["frequency"]
         gwecc_dict = measure_eccentricity(
             fref_in=fref_in,
@@ -67,8 +66,9 @@ def test_regression():
         # Compare the measured data with the saved data
         np.testing.assert_allclose(
             ecc_ref, regression_data_at_fref["eccentricity"],
+            atol=atol,
             err_msg="measured and saved eccentricity at saved frequencies do not match.")
         np.testing.assert_allclose(
             meanano_ref, regression_data_at_fref["mean_anomaly"],
-            atol=mean_anomaly_atol,
+            atol=atol,
             err_msg="measured and saved mean anomaly at saved frequencies do not match.")
