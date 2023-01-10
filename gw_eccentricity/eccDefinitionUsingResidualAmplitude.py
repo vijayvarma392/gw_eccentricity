49e56e524128b777f8b77895eac3ef9d98fe4e4e
md-arif-shaikh <arifshaikh.astro@gmail.com>
2023-01-10 11:05:56 +0530

    add doc, concise func

--- gw_eccentricity/eccDefinitionUsingResidualAmplitude.py
+++ gw_eccentricity/eccDefinitionUsingResidualAmplitude.py
@@ -28,13 +28,21 @@ class eccDefinitionUsingResidualAmplitude(eccDefinitionUsingAmplitude):
         self.label_for_data_for_finding_extrema = r"$\Delta A_{22}$"
 
     def check_and_raise_zeroecc_data_not_found(self, method):
-        """Raise exception if zeroecc data not found for Residual method."""
-        if "hlm_zeroecc" not in self.dataDict:
-            raise Exception(f"Method {method} must have zeroecc data in "
-                            "dataDict. 'hlm_zeroecc' data not found.")
-        if "t_zeroecc" not in self.dataDict:
-            raise Exception(f"Method {method} must have zeroecc data in "
-                            "dataDict. 't_zeroecc' data not found.")
+        """Raise exception if zeroecc data not found for Residual method.
+
+        For Residual methods, waveform data of the quasicircular counterpart
+        is required to compute residual amplitude/frequency which is then
+        used to find the locations of the pericenters/apocenters.
+
+        In the dataDict, these are provided using the keys
+        - t_zeroecc: 1d array of times of quasicircular waveform.
+        - hlm_zeroecc: Dictionary of modes of the quasicircular waveform.
+        For more details on the format of the dataDict, see documentation
+        of gw_eccentricity.measure_eccentricity."""
+        for k in ["t_zeroecc", "hlm_zeroecc"]:
+            if k is not in self.dataDict:
+                raise Exception(f"Method {method} must have zeroecc data in "
+                                "dataDict. {k} data not found.")
 
     def get_data_for_finding_extrema(self):
         """Get the data for extrema finding."""
