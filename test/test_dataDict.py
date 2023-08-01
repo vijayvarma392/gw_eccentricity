d69fe571113f76505fb32294201c0849ef161f10
md-arif-shaikh <arifshaikh.astro@gmail.com>
2023-08-01 15:31:03 +0900

    make variables name consistent

--- test/test_dataDict.py
+++ test/test_dataDict.py
@@ -26,6 +26,14 @@ def test_dataDict():
     amplm = {}
     omegalm = {}
     dt = dataDict["t"][1] - dataDict["t"][0]
+    # check that the time array is uniform. This is required to obtain omega by
+    # taking derivative of phase applying time_deriv_4thorder.
+    ids = np.where(np.abs(np.diff(dataDict["t"]) - dt) > 1e-7)[0]
+    if len(ids) > 0:
+        dt_ = np.diff(dataDict["t"])[ids[0]]
+        raise Exception("t_zeroecc is not uniform. dt at the start of the "
+                        f"time array is {dt}. dt={dt_} between {ids[0]} and "
+                        f"{ids[0]+1}. The difference is {dt - dt_}")
     for k in dataDict["hlm"]:
         phaselm[k] = -np.unwrap(np.angle(dataDict["hlm"][k]))
         amplm[k] = np.abs(dataDict["hlm"][k])
@@ -36,6 +44,15 @@ def test_dataDict():
     amplm_zeroecc = {}
     omegalm_zeroecc = {}
     dt_zeroecc = dataDict["t_zeroecc"][1] - dataDict["t_zeroecc"][0]
+    # check that t_zeroecc is uniform
+    ids = np.where(np.abs(np.diff(dataDict["t_zeroecc"]) - dt_zeroecc)
+                   > 1e-7)[0]
+    if len(ids) > 0:
+        dt_zeroecc_ = np.diff(dataDict["t_zeroecc"])[ids[0]]
+        raise Exception("t_zeroecc is not uniform. dt at the start of the "
+                        f"time array is {dt_zeroecc}. dt={dt_zeroecc_} "
+                        f"between {ids[0]} and {ids[0]+1}. The difference is "
+                        f"{dt_zeroecc - dt_zeroecc_}")
     for k in dataDict["hlm_zeroecc"]:
         phaselm_zeroecc[k] = -np.unwrap(np.angle(dataDict["hlm_zeroecc"][k]))
         amplm_zeroecc[k] = np.abs(dataDict["hlm_zeroecc"][k])
@@ -46,10 +63,11 @@ def test_dataDict():
     available_methods = gw_eccentricity.get_available_methods()
 
     # create data dict with hlm
-    data_hlm = {"t": dataDict["t"],
-                "hlm": dataDict["hlm"]}
+    data_w_hlm = {
+        "t": dataDict["t"],
+        "hlm": dataDict["hlm"]}
     # create data dict with amplm and phaselm
-    data_amplm_phaselm = {
+    data_w_amplm_phaselm = {
         "t": dataDict["t"],
         "amplm": amplm,
         "phaselm": phaselm}
@@ -58,56 +76,57 @@ def test_dataDict():
         # Using hlm
         if "Residual" in method:
             # add zeroecc hlm
-            data_hlm.update({"t_zeroecc": dataDict["t_zeroecc"],
-                             "hlm_zeroecc": dataDict["hlm_zeroecc"]})
-        return_dict_hlm = measure_eccentricity(
+            data_w_hlm.update({
+                "t_zeroecc": dataDict["t_zeroecc"],
+                "hlm_zeroecc": dataDict["hlm_zeroecc"]})
+        result_w_hlm = measure_eccentricity(
             tref_in=dataDict["t"],
             method=method,
-            dataDict=data_hlm)
-        gwecc_object_hlm = return_dict_hlm["gwecc_object"]
-        eccentricity_w_hlm = gwecc_object_hlm.eccentricity
-        mean_anomaly_w_hlm = gwecc_object_hlm.mean_anomaly
+            dataDict=data_w_hlm)
+        gwecc_object_w_hlm = result_w_hlm["gwecc_object"]
+        eccentricity_w_hlm = gwecc_object_w_hlm.eccentricity
+        mean_anomaly_w_hlm = gwecc_object_w_hlm.mean_anomaly
 
         # Using hlm with omegalm also provided
-        data_hlm.update({"omegalm": omegalm})
+        data_w_hlm.update({"omegalm": omegalm})
         if "Residual" in method:
-            data_hlm.update(
+            data_w_hlm.update(
                 {"omegalm_zeroecc": omegalm_zeroecc})
-        return_dict_hlm_omegalm = measure_eccentricity(
+        result_w_hlm_omegalm = measure_eccentricity(
             tref_in=dataDict["t"],
             method=method,
-            dataDict=data_hlm)
-        gwecc_object_hlm_omegalm = return_dict_hlm_omegalm["gwecc_object"]
-        eccentricity_w_hlm_and_omegalm = gwecc_object_hlm_omegalm.eccentricity
-        mean_anomaly_w_hlm_and_omegalm = gwecc_object_hlm_omegalm.mean_anomaly
+            dataDict=data_w_hlm)
+        gwecc_object_w_hlm_omegalm = result_w_hlm_omegalm["gwecc_object"]
+        eccentricity_w_hlm_and_omegalm = gwecc_object_w_hlm_omegalm.eccentricity
+        mean_anomaly_w_hlm_and_omegalm = gwecc_object_w_hlm_omegalm.mean_anomaly
 
         # using amplm and phaselm
         if "Residual" in method:
             # add zeroecc amplm and phaselm
-            data_amplm_phaselm.update(
+            data_w_amplm_phaselm.update(
                 {"t_zeroecc": dataDict["t_zeroecc"],
                  "amplm_zeroecc": amplm_zeroecc,
                  "phaselm_zeroecc": phaselm_zeroecc})
-        return_dict_amplm_phaselm = measure_eccentricity(
+        result_w_amplm_phaselm = measure_eccentricity(
             tref_in=dataDict["t"],
             method=method,
-            dataDict=data_hlm)
-        gwecc_object_amplm_phaselm = return_dict_amplm_phaselm["gwecc_object"]
-        eccentricity_w_amplm_phaselm = gwecc_object_amplm_phaselm.eccentricity
-        mean_anomaly_w_amplm_phaselm = gwecc_object_amplm_phaselm.mean_anomaly
+            dataDict=data_w_hlm)
+        gwecc_object_w_amplm_phaselm = result_w_amplm_phaselm["gwecc_object"]
+        eccentricity_w_amplm_phaselm = gwecc_object_w_amplm_phaselm.eccentricity
+        mean_anomaly_w_amplm_phaselm = gwecc_object_w_amplm_phaselm.mean_anomaly
 
         # Using amplm and phaselm with omegalm
-        data_amplm_phaselm.update({"omegalm": omegalm})
+        data_w_amplm_phaselm.update({"omegalm": omegalm})
         if "Residual" in method:
-            data_amplm_phaselm.update(
+            data_w_amplm_phaselm.update(
                 {"omegalm_zeroecc": omegalm_zeroecc})
-        return_dict_amplm_phaselm_and_omegalm = measure_eccentricity(
+        result_w_amplm_phaselm_and_omegalm = measure_eccentricity(
             tref_in=dataDict["t"],
             method=method,
-            dataDict=data_hlm)
-        gwecc_object_amplm_phaselm_and_omegalm = return_dict_amplm_phaselm_and_omegalm["gwecc_object"]
-        eccentricity_w_amplm_phaselm_and_omegalm = gwecc_object_amplm_phaselm_and_omegalm.eccentricity
-        mean_anomaly_w_amplm_phaselm_and_omegalm = gwecc_object_amplm_phaselm_and_omegalm.mean_anomaly
+            dataDict=data_w_hlm)
+        gwecc_object_w_amplm_phaselm_and_omegalm = result_w_amplm_phaselm_and_omegalm["gwecc_object"]
+        eccentricity_w_amplm_phaselm_and_omegalm = gwecc_object_w_amplm_phaselm_and_omegalm.eccentricity
+        mean_anomaly_w_amplm_phaselm_and_omegalm = gwecc_object_w_amplm_phaselm_and_omegalm.mean_anomaly
 
         # Compare eccenrtcity and mean anomaly between hlm and amp/phase
         np.testing.assert_allclose(eccentricity_w_hlm,
